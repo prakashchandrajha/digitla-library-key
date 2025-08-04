b4240cf2ee024f78ecf22f3cec1eed65a2702b73
Your Name <your.email@example.com>
2025-08-04 17:34:01 +0530

    qqq

--- src/app/keycloak.config.ts
+++ src/app/keycloak.config.ts
@@ -3,5 +3,5 @@ import { KeycloakConfig } from 'keycloak-js';
 export const keycloakConfig: KeycloakConfig = {
   url: 'http://localhost:8180', // Your Keycloak server URL
   realm: 'digital-library', // Your realm name
-  clientId: 'digital-library-frontend', // Your client ID
+  clientId: 'digital-library-frontend', // Your client I
 }; 
